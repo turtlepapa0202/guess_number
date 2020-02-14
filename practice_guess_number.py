139900c2165ecc5ab8c6021720f341e16fefa7b7
turtlepapa0202 <enlinkuo@gmail.com>
2020-02-14 15:04:42 +0800

    Update new file

--- practice_guess_number.py
+++ practice_guess_number.py
@@ -2,17 +2,31 @@
 # 讓使用者重複輸入數字去猜
 # 猜對的話，印出"終於猜對了！"
 # 猜錯的話，要告訴他，比答案大/小
+# bonus：邊猜邊顯示猜了第幾次
 
 import random
-number = random.randint(1, 100)
-
+a = input('請決定隨機數字範圍開始值: ')
+a = int(a)
+b = input('請決定隨機數字範圍結束值: ')
+b = int(b)
+number = random.randint(a, b)
+x = 1
 while True:
-	guess_num = input('請猜1~100的數字: ')
+	guess_num = input('請猜範圍內數字: ')
 	guess_num = int(guess_num)
 	if guess_num == number:
-		print('終於猜對了！')
+		if x == 1:
+			print('厲害，一次就猜對！')
+		else:
+			print('終於猜對了！')
+			print('總共猜了', x, '次')
 		break
 	elif guess_num > number:
 		print('比答案大')
 	elif guess_num < number:
 		print('比答案小')
+	
+	print('目前猜了', x, '次')
+	print('')
+	x += 1
+	
